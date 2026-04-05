59b168e6d8166cc64d34a119293741e679cc7f6b
Varaprasad Erapu <57343469+VaraprasadE@users.noreply.github.com>
2026-04-05 10:36:01 +0530

    Refactor circular buffer for overwriting and size change
    Updated circular buffer implementation to support overwriting and changed buffer size to a power of 2.

--- Data-Structures/circularBuffer.c
+++ Data-Structures/circularBuffer.c
@@ -1,89 +1,50 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include<stdio.h>
+#include<stdbool.h>
 
-#define BUFFER_SIZE 5
+#define BUFFER_SIZE 8          // Buffer Size should be 2 power value
+#define MASK (BUFFER_SIZE - 1)
 
-typedef struct circularBuffer {
-    int buffer[BUFFER_SIZE];
-    int head;
-    int tail;
-    int count;
+typedef struct circularBuffer{
+	int buffer[BUFFER_SIZE];
+	int head;
+	int tail;
 } circularBuffer;
 
-circularBuffer trainBuffer = {0};
-
-// No overwriting of data in circular buffer, so we check if the buffer is full before pushing new element
-bool pushCircularBufferNoOverwrite(circularBuffer *cb, int element){
-    if(cb->count == BUFFER_SIZE){
-       printf("Buffer is full\n");
-       return false;
-    }
-    cb->buffer[cb->head] = element;
-    cb->head = (cb->head + 1) % BUFFER_SIZE;
-    cb->count++;
-    return true;
+bool pushBuffer(circularBuffer *cb, int *val){
+	if(((cb->head + 1) & MASK) == cb->tail){
+		printf("Buffer Full \n");
+		return 0;
+	}
+	cb->buffer[cb->head] = *val;
+	cb->head = (cb->head + 1) & MASK;
+	return 1;
 }
 
-int popCircularBufferNoOverwrite(circularBuffer *cb){
-    if(cb->count == 0){
-       printf("Buffer is Empty\n");
-       return -1;
-    }
-    int element = cb->buffer[cb->tail];
-    cb->tail = (cb->tail + 1) % BUFFER_SIZE;
-    cb->count--;
-    return element;
+int popBuffer(circularBuffer *cb){
+	if(cb->head == cb->tail){
+		printf("Buffer Empty \n");
+		return 0;
+	}
+	int val = cb->buffer[cb->tail];
+	cb->tail = (cb->tail + 1) & MASK;
+	return val;
 }
 
-// Overwriting of data in circular buffer, so we push new element even if the buffer is full, and overwrite the oldest element
-void pushCircularBufferOverwrite(circularBuffer *cb, int element){
-    if(cb->count == BUFFER_SIZE){
-       printf("Buffer is full, overwriting oldest element\n");
-       cb->tail = (cb->tail + 1) % BUFFER_SIZE; // Move tail to next position to overwrite the oldest element
-    } else {
-       cb->count++;
-    }
-    cb->buffer[cb->head] = element;
-    cb->head = (cb->head + 1) % BUFFER_SIZE;
+void initBuffer(circularBuffer *cb){
+	cb->head = 0;
+	cb->tail = 0;
 }
 
-int popCircularBufferOverwrite(circularBuffer *cb){
-    if(cb->count == 0){
-       printf("Buffer is Empty\n");
-       return -1;
-    }
-    int element = cb->buffer[cb->tail];
-    cb->tail = (cb->tail + 1) % BUFFER_SIZE;
-    cb->count--;
-    return element;
-}
+void main(){
+	circularBuffer cb;
+	initBuffer(&cb);
+	
+	for(int i=0; i<15; i++){
+	    pushBuffer(&cb, &i);
+	}
 
-int main(){
-    // pushCircularBufferNoOverwrite(&trainBuffer, 1);
-    // pushCircularBufferNoOverwrite(&trainBuffer, 2);
-    // pushCircularBufferNoOverwrite(&trainBuffer, 3);
-    // pushCircularBufferNoOverwrite(&trainBuffer, 4);
-    // pushCircularBufferNoOverwrite(&trainBuffer, 5);
-    // pushCircularBufferNoOverwrite(&trainBuffer, 6); // This will show buffer is full
-    // printf("Popped element: %d\n", popCircularBufferNoOverwrite(&trainBuffer)); // Output will be 1
-    // printf("Popped element: %d\n", popCircularBufferNoOverwrite(&trainBuffer)); // Output will be 2
-    // printf("Popped element: %d\n", popCircularBufferNoOverwrite(&trainBuffer)); // Output will be 3
-    // printf("Popped element: %d\n", popCircularBufferNoOverwrite(&trainBuffer)); // Output will be 4
-    // printf("Popped element: %d\n", popCircularBufferNoOverwrite(&trainBuffer)); // Output will be 5
-    // printf("Popped element: %d\n", popCircularBufferNoOverwrite(&trainBuffer)); // This will show buffer is empty
-    printf("Testing circular buffer with overwriting:\n");
-    pushCircularBufferOverwrite(&trainBuffer, 1);
-    pushCircularBufferOverwrite(&trainBuffer, 2);
-    pushCircularBufferOverwrite(&trainBuffer, 3);
-    pushCircularBufferOverwrite(&trainBuffer, 4);
-    pushCircularBufferOverwrite(&trainBuffer, 5);
-    pushCircularBufferOverwrite(&trainBuffer, 6); // This will overwrite the oldest element
-    printf("Popped element: %d\n", popCircularBufferOverwrite(&trainBuffer)); // Output will be 2
-    printf("Popped element: %d\n", popCircularBufferOverwrite(&trainBuffer));
-    printf("Popped element: %d\n", popCircularBufferOverwrite(&trainBuffer));
-    printf("Popped element: %d\n", popCircularBufferOverwrite(&trainBuffer));
-    printf("Popped element: %d\n", popCircularBufferOverwrite(&trainBuffer)); // Output will be 6
-    printf("Popped element: %d\n", popCircularBufferOverwrite(&trainBuffer)); // This will show buffer is empty
-    return 0;
-   
+	for(int i=0; i<15; i++){
+	    popBuffer(&cb);
+	}
+	
 }
